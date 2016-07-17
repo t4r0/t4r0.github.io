04b5972d10e886f6c6271f47f273c10e9b3d0f46
Estuardo Diaz <esjoando555@gmail.com>
2016-07-17 10:01:22 -0600

    New folder structure

--- gulp/build.js
+++ gulp/build.js
@@ -90,7 +90,14 @@ gulp.task('other', function () {
 });
 
 gulp.task('clean', function () {
-  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+  return $.del([
+    'index.html',
+    'favicon.ico',
+    'assets/**/*',
+    'scripts/**/*',
+    'styles/**/*',
+    path.join(conf.paths.tmp, '/')
+  ]);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
