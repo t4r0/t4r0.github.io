65d8611d0b7ca872c07fcff98642ba95a83fd343
Estuardo Diaz <esjoando555@gmail.com>
2016-07-18 22:04:32 -0600

    updates my info

--- src/app/controllers/scroll-animate.js
+++ src/app/controllers/scroll-animate.js
@@ -5,26 +5,11 @@
     .controller('ScrollAnimateController', ['$scope', '$document',function($scope, $document){
       //returns the scroll position of the window
       $scope.getScrollOffset = function(w){
-        w = w || window;
-
-        if (w.pageXOffset !== null) {
-          return {
-            x: w.pageXOffset,
-            y: w.pageYOffset
-          };
-        }
-
-        if ($document.compatMode === 'CSS1Compat'){
-          return {
-            x: $document.documentElement.scrollLeft,
-            y: $document.documentElement.scrollTop
-          };
-        }
-
         return {
-          x: d.body.scrollLeft,
-          y: d.body.scrollTop
+          x: w.scrollX,
+          y: w.scrollY
         };
+
       };
 
       $scope.getPosition = function(e){
@@ -36,8 +21,8 @@
 
       $scope.getViewportSize = function(w) {
         return {
-                x: Math.max($document.width() || 0),
-                y: Math.max($document.height() || 0)
+                x: Math.max( $document[0].documentElement.clientWidth, w.innerWidth || 0),
+                y: Math.max( $document[0].documentElement.clientHeight, w.innerHeight || 0)
         };
       };
     }]);
